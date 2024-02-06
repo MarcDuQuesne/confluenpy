9320262cd273e47c953c4f4a247ff7bf79746499
Matteo Giani <matteo.giani@dsm.com>
2024-02-06 15:58:29 +0000

    Added e2e test.

---
+++ tests/test_confluence.py
@@ -0,0 +1,52 @@
+"""
+E2e test, publishing a page to confluence
+"""
+import os
+from pathlib import Path
+
+import atlassian
+import pytest
+
+from confluenpy.content import Page, PageContent
+from confluenpy.markdown import MarkdownToConfluenceConverter
+
+
+@pytest.fixture
+def confluence():
+    """
+    Confluence instance
+    """
+    return atlassian.Confluence(
+        url=os.getenv("CONFLUENCE_URL"), username=os.getenv("CONFLUENCE_USERNAME"), password=os.getenv("CONFLUENCE_API_TOKEN")
+    )
+
+
+@pytest.mark.end_to_end
+def test_publish_page(confluence: atlassian.Confluence):
+    """
+    Test publishing a page to confluence
+
+    To run this, make sure you have a .env file with the following variables:
+    CONFLUENCE_URL: The url for your confluence
+    CONFLUENCE_USER: Your confluence username
+    CONFLUENCE_API_TOKEN: https://id.atlassian.com/manage-profile/security/api-tokens
+    CONFLUENCE_PAGE: a page in your confluence
+    CONFLUENCE_SPACE: a space in your confluence
+    """
+
+    page = Page(title=os.getenv("CONFLUENCE_PAGE"), space=os.getenv("CONFLUENCE_SPACE"), confluence=confluence)
+
+    # Add a table of contents
+    page.body.toc()
+    # Add a horizontal rule
+    page.body.horizontal_rule()
+
+    # Add the readme
+    readme = Path(__file__).parent.parent / "README.md"
+    with readme.open(encoding="utf-8") as markdown_text:
+        markdown = MarkdownToConfluenceConverter.convert(markdown_text.read())
+        page.body.extend(markdown)
+
+    # Update the page
+    status = page.update()
+    assert status["type"] == "page", status
